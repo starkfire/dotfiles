9af4b53614d9d763e70dbb671a6a6e3cdf89b644
starkfire <plpantoja49@gmail.com>
2024-01-26 11:17:49 +0800

    fix: remove unused config

--- nvim/windows/lua/plugins/telescope.lua
+++ nvim/windows/lua/plugins/telescope.lua
@@ -1,54 +1,3 @@
-local config = {
-    TelescopeBorder = {
-        fg = '#16161e',
-        bg = '#16161e'
-    },
-    TelescopePromptBorder = {
-        fg = "#1f2336",
-        bg = "#1f2336"
-    },
-    TelescopeNormal = {
-        bg = '#16161e'
-    }
-}
-
-local tokyonightConfig = {
-    -- borderless telescope
-    defaults = {
-        on_highlights = function(hl, c)
-            -- local prompt = "#2d3149"
-            local prompt = "#99154a"
-            hl.TelescopeNormal = {
-                bg = c.bg_dark,
-                fg = c.fg_dark,
-            }
-            hl.TelescopeBorder = {
-                bg = c.bg_dark,
-                fg = c.bg_dark,
-            }
-            hl.TelescopePromptNormal = {
-                bg = prompt,
-            }
-            hl.TelescopePromptBorder = {
-                bg = prompt,
-                fg = prompt
-            }
-            hl.TelescopePromptTitle = {
-                bg = prompt,
-                fg = prompt
-            }
-            hl.TelescopePreviewTitle = {
-                bg = c.bg_dark,
-                fg = c.bg_dark,
-            }
-            hl.TelescopeResultsTitle = {
-                bg = c.bg_dark,
-                fg = c.bg_dark,
-            }
-        end,
-    }
-}
-
 local telescopeConfig = {
     defaults = {
         theme = "center",
@@ -60,24 +9,7 @@ local telescopeConfig = {
                 preview_width = 0.5,
                 results_width = 0.8
             }
-        },
-        on_highlights = function(hl, c)
-            local prompt = '#99154a'
-            hl.TelescopeBorder = {
-                bg = prompt,
-                fg = prompt
-            }
-            hl.TelescopeNormal = {
-                bg = prompt,
-                fg = prompt
-            }
-            hl.TelescopePromptNormal = {
-                bg = prompt
-            }
-            hl.TelescopePromptBorder = {
-                bg = prompt
-            }
-        end,
+        }
     },
     pickers = {
         find_files = {
